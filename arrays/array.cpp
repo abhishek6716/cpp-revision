cbcfa9331e7cd5af65c8e6a617fb20a5a52b4f48
Abhishek singh <abhisheksingh6716@gmail.com>
2021-03-27 00:28:11 +0530

    added more problems

--- arrays/array.cpp
+++ arrays/array.cpp
@@ -71,9 +71,178 @@ void pairSum(int arr[], int n, int sum)
     }
 }
 
+void sort01(int arr[], int n)
+{
+    int i = 0;
+    int nextZero = 0;
+    int nextTwo = n - 1;
+    while (i <= nextTwo)
+    {
+        if (arr[i] == 0)
+        {
+            int temp = arr[i];
+            arr[i] = arr[nextZero];
+            arr[nextZero] = temp;
+            i++;
+            nextZero++;
+        }
+        else if (arr[i] == 2)
+        {
+            int temp = arr[i];
+            arr[i] = arr[nextTwo];
+            arr[nextTwo] = temp;
+            nextTwo--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+void sumOfTwoArrays(int *input1, int size1, int *input2, int size2, int *output)
+{
+    int i = size1 - 1;
+    int j = size2 - 1;
+    int carry = 0;
+    int k = max(size1, size2);
+
+    while (i >= 0 && j >= 0)
+    {
+        int sum = input1[i] + input2[j] + carry;
+        output[k] = sum % 10;
+        carry = sum / 10;
+        i--;
+        j--;
+        k--;
+    }
+
+    while (i >= 0)
+    {
+        int sum = input1[i] + carry;
+        output[k] = sum % 10;
+        carry = sum / 10;
+        i--;
+        k--;
+    }
+
+    while (j >= 0)
+    {
+        int sum = input2[j] + carry;
+        output[k] = sum % 10;
+        carry = sum / 10;
+        j--;
+        k--;
+    }
+}
+
+void pushZeoresToEnd(int arr[], int n)
+{
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+    for (; k < n; k++)
+    {
+        arr[k] = 0;
+    }
+}
+
+void rotateArray(int arr[], int n, int d)
+{
+    int r = 1;
+    while (r <= d)
+    {
+        int temp = arr[0];
+        for (int i = 0; i < n - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = temp;
+        r++;
+    }
+}
+
+int secondLargest(int arr[], int n)
+{
+    int max1, max2;
+    max1 = max2 = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max1)
+        {
+            max2 = max1;
+            max1 = arr[i];
+        }
+        if (arr[i] > max2 && arr[i] < max1)
+        {
+            max2 = arr[i];
+        }
+    }
+    return max2;
+}
+
+int arrRotation(int arr[], int n)
+{
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return i + 1;
+        }
+    }
+}
+
+void sumOfTwoArrays(int *input1, int size1, int *input2, int size2, int *output)
+{
+    int i = size1 - 1;
+    int j = size2 - 1;
+    int carry = 0;
+    int k = max(size1, size2);
+
+    while (i >= 0 && j >= 0)
+    {
+        int sum = input1[i] + input2[j] + carry;
+        output[k] = sum % 10;
+        carry = sum / 10;
+        i--;
+        j--;
+        k--;
+    }
+
+    while (i >= 0)
+    {
+        int sum = input1[i] + carry;
+        output[k] = sum % 10;
+        carry = sum / 10;
+        i--;
+        k--;
+    }
+
+    while (j >= 0)
+    {
+        int sum = input2[j] + carry;
+        output[k] = sum % 10;
+        carry = sum / 10;
+        j--;
+        k--;
+    }
+}
+
 int main()
 {
-    int arr[] = {0, 7, 2, 5, 4, 7, 1, 3, 6};
+    int arr[] = {0, 1, 1, 0, 0, 1, 1, 0, 0};
     int n = 7;
-    cout << findDup(arr, n);
+    sort01(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return EXIT_SUCCESS;
 }
